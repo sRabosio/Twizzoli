155ea8fd319ed77383258909f65d55734b165bab
itscailor <l.cairo@itsrizzoli.it>
2022-12-20 12:50:29 +0100

    aggiunto controllo in base a input

--- app/src/main/java/it/itsar/twizzoli/MainActivity.java
+++ app/src/main/java/it/itsar/twizzoli/MainActivity.java
@@ -1,42 +1,53 @@
 package it.itsar.twizzoli;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
 
-    private String username ="federico";
-    private String password="111";
+    private String username ="user";
+    private String password="pass";
     private Button login;
     private Button registrati;
     private EditText usernameinput;
-    private TextInputEditText passwordinput;
+    private EditText passwordinput;
+    private String us;
+    private String ps;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login = findViewById(R.id.loginbutton);
         registrati = findViewById(R.id.button);
-        usernameinput.setText(getText(R.id.usernameinput).toString().trim());
+        usernameinput = findViewById(R.id.usernameinput);
+        passwordinput = findViewById(R.id.passwordinput);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(username=="federico"){
-                    if(password=="111"){
+                us = usernameinput.getText().toString();
+                ps = passwordinput.getText().toString();
+                Log.d(TAG, us);
+                Log.d(TAG, ps);
+                if(Objects.equals(us, "user")){
+                    if(Objects.equals(ps, "pass")){
                         Intent intent = new Intent(MainActivity.this,Homepage.class);
                         intent.putExtra("nome","lorenzo");
                         startActivity(intent);
                     }
-
                 }
                 else{
                     //messaggio di errore
