04525999e37716e42e9258caa51dc9af0b0a18be
sRabosio <rabosiosimone@gmail.com>
2023-01-04 12:16:50 +0100

    post fragment layout

---
+++ app/src/main/java/it/itsar/twizzoli/fragments/Post.java
@@ -0,0 +1,17 @@
+package it.itsar.twizzoli.fragments;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+
+public class Post extends Fragment {
+
+
+    public Post() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
