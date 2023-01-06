b6725a16e3ef003f582dfdcf8300b05d6dc405dc
sRabosio <rabosiosimone@gmail.com>
2023-01-06 19:13:48 +0100

    new post animation

--- app/src/main/java/it/itsar/twizzoli/Homepage.java
+++ app/src/main/java/it/itsar/twizzoli/Homepage.java
@@ -1,27 +1,27 @@
 package it.itsar.twizzoli;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator;
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.animation.LinearInterpolator;
 
-import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
-import it.itsar.twizzoli.adapters.PostListAdapter;
 import it.itsar.twizzoli.controller.AppController;
 import it.itsar.twizzoli.data.PostRepo;
 import it.itsar.twizzoli.databinding.ActivityHomepageBinding;
 import it.itsar.twizzoli.fragments.Feed;
-import it.itsar.twizzoli.models.Post;
+import it.itsar.twizzoli.fragments.NewPost;
 import it.itsar.twizzoli.models.User;
 
 //TODO: implement new post fragment
@@ -34,17 +34,36 @@ public class Homepage extends AppCompatActivity {
     private PostRepo postRepo = new PostRepo();
     private User loggedUser = null;
     private BottomNavigationView bottomAppBar;
+    private float newpostTranslationValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
         if (!loggedUserChecks()) return;
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_homepage);
         bottomAppBar = binding.bottombar;
+        newpostTranslationValue = binding.homepageNewpostCard.getTranslationY();
+        switchNewPostFragment();
         switchFragment(Feed.class);
+        containerMain();
         bottomAppBar();
+    }
+
+    private void containerMain(){
+        final View container = binding.containerMain;
+        container.setOnClickListener(view->{
+            if(binding.homepageNewpostCard.getTranslationY() == 0)
+                moveNewPost();
+        });
+    }
+
+    private void switchNewPostFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.homepage_newpost, NewPost.class, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("new post")
+                .commit();
     }
 
     private void bottomAppBar(){
@@ -57,6 +76,10 @@ public class Homepage extends AppCompatActivity {
                     switchFragment(Feed.class);
                     break;
 
+                case newpost:
+                    moveNewPost();
+                    break;
+
                 case user:
                     break;
                 default:
@@ -66,6 +89,20 @@ public class Homepage extends AppCompatActivity {
 
             return true;
         });
+    }
+
+    private void moveNewPost(){
+        CardView newpostCard = binding.homepageNewpostCard;
+        float translationYValue = 0;
+
+        if(newpostCard.getTranslationY() == 0)
+            translationYValue = newpostTranslationValue;
+
+        newpostCard.animate()
+                .setDuration(500)
+                .translationY(translationYValue)
+                .setInterpolator(new FastOutSlowInInterpolator())
+                .start();
     }
 
     private <T extends Fragment> void switchFragment(Class<T> fragment){
--- app/src/main/java/it/itsar/twizzoli/fragments/NewPost.java
+++ app/src/main/java/it/itsar/twizzoli/fragments/NewPost.java
@@ -1,12 +1,22 @@
-package it.itsar.twizzoli;
+package it.itsar.twizzoli.fragments;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import it.itsar.twizzoli.R;
+import it.itsar.twizzoli.controller.AppController;
+import it.itsar.twizzoli.models.User;
+
 public class NewPost extends Fragment {
+
+    private User loggedUser = null;
+    private final AppController controller = AppController.getInstance();
 
     public NewPost() {
         // Required empty public constructor
@@ -17,6 +27,7 @@ public class NewPost extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loggedUser = controller.getLoggedUser();
     }
 
     @Override
@@ -24,5 +35,11 @@ public class NewPost extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_new_post, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
