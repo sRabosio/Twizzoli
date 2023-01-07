ff317da400b3a8ffb06f288f5d288bccb5b53166
sRabosio <rabosiosimone@gmail.com>
2023-01-07 12:33:05 +0100

    post activity layout

---
+++ app/src/main/java/it/itsar/twizzoli/fragments/CommentFragment.java
@@ -0,0 +1,30 @@
+package it.itsar.twizzoli.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import it.itsar.twizzoli.R;
+
+public class CommentFragment extends Fragment {
+
+    public CommentFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_comment, container, false);
+    }
+}
