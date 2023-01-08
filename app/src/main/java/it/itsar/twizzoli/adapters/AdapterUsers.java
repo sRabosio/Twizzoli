63a6e2a51256cfcdc52d4e12e3976374bb83e35d
sRabosio <rabosiosimone@gmail.com>
2023-01-08 13:46:01 +0100

    gradle fix

--- app/src/main/java/it/itsar/twizzoli/adapters/AdapterUsers.java
+++ app/src/main/java/it/itsar/twizzoli/adapters/AdapterUsers.java
@@ -11,8 +11,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
 import java.util.List;
 
