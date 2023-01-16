2318a18b57eb211e0bfea5fa5fc4b4f42e5a0da8
sRabosio <rabosiosimone@gmail.com>
2023-01-16 10:30:52 +0100

    user checks

--- app/src/main/java/it/itsar/twizzoli/Registrazione.java
+++ app/src/main/java/it/itsar/twizzoli/Registrazione.java
@@ -7,16 +7,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.checkerframework.framework.qual.DefaultQualifier;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import it.itsar.twizzoli.databinding.ActivityRegistrazioneBinding;
+import it.itsar.twizzoli.models.User;
 
 public class Registrazione extends AppCompatActivity {
 
@@ -70,8 +74,26 @@ public class Registrazione extends AppCompatActivity {
             toRegister.put("follower", new ArrayList<>());
             toRegister.put("following", new ArrayList<>());
 
+            final User user = new User();
+            user.username = username;
+            user.email = email;
+            user.password = password;
+
+            userRef.get().addOnSuccessListener(snap->{
+                ArrayList<User> users = new ArrayList<>(snap.toObjects(User.class));
+                if(users.contains(user)){
+                    Toast.makeText(this, "User already exists!", Toast.LENGTH_SHORT)
+                            .show();
+                    return;
+                }
+                setUser(user);
+            });
+        });
+    }
+
+        private void setUser (User toRegister){
             userRef
-                    .document(username)
+                    .document(toRegister.username)
                     .set(toRegister)
                     .addOnSuccessListener(documentReference -> {
                         Intent intent = new Intent(Registrazione.this, LoginActivity.class);
@@ -79,52 +101,51 @@ public class Registrazione extends AppCompatActivity {
                         startActivity(intent);
                     })
                     .addOnFailureListener(e -> Snackbar.make(v, "" + e.getMessage(), 3000).show());
-        });
-    }
+        }
 
 
-    private void passwordCheck() {
-        binding.passwordConf.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        private void passwordCheck () {
+            binding.passwordConf.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                String passConfText = binding.passwordConf.toString();
-
-                if (passConfText.isEmpty() || isPasswordOK()) {
-                    binding.passwordConfLabel.setTextColor(getColor(android.R.color.black));
-                    return;
                 }
-                binding.passwordConfLabel.setTextColor(getColor(android.R.color.holo_red_light));
-            }
-        });
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+                    String passConfText = binding.passwordConf.toString();
+
+                    if (passConfText.isEmpty() || isPasswordOK()) {
+                        binding.passwordConfLabel.setTextColor(getColor(android.R.color.black));
+                        return;
+                    }
+                    binding.passwordConfLabel.setTextColor(getColor(android.R.color.holo_red_light));
+                }
+            });
+        }
+
+        private boolean isPasswordOK () {
+            String passwordText = binding.password.getText().toString();
+            String passConfText = binding.passwordConf.getText().toString();
+            return passConfText.equals(passwordText) && !(passConfText.isEmpty() || passwordText.isEmpty());
+        }
+
+        private boolean isInputOk () {
+            return isPasswordOK() && !isInputEmpty();
+        }
+
+        private boolean isInputEmpty () {
+            String email = binding.email.getText().toString();
+            String passwordConf = binding.passwordConf.getText().toString();
+            String password = binding.password.getText().toString();
+            String username = binding.username.getText().toString();
+
+            return email.isEmpty() || passwordConf.isEmpty() || password.isEmpty() || username.isEmpty();
+        }
+
+
     }
-
-    private boolean isPasswordOK() {
-        String passwordText = binding.password.getText().toString();
-        String passConfText = binding.passwordConf.getText().toString();
-        return passConfText.equals(passwordText) && !(passConfText.isEmpty() || passwordText.isEmpty());
-    }
-
-    private boolean isInputOk() {
-        return isPasswordOK() && !isInputEmpty();
-    }
-
-    private boolean isInputEmpty() {
-        String email = binding.email.getText().toString();
-        String passwordConf = binding.passwordConf.getText().toString();
-        String password = binding.password.getText().toString();
-        String username = binding.username.getText().toString();
-
-        return email.isEmpty() || passwordConf.isEmpty() || password.isEmpty() || username.isEmpty();
-    }
-
-
-}
